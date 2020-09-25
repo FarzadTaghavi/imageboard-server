6662721ba629c3bce77dc1801eb1d98d2cd87fe7
farzadtaghavi <fmtaghavi@gmail.com>
2020-09-25 22:58:30 +0200

    cleaned up

--- routers/images.js
+++ routers/images.js
@@ -1,24 +1,28 @@
 const { Router } = require("express");
 const router = new Router();
-const { toData } = require("../auth/jwt");
 
 const Images = require("../models").image;
 
 router.get("/", async (req, res, next) => {
-  const auth =
-    req.headers.authorization && req.headers.authorization.split(" ");
-  if (auth && auth[0] === "Bearer" && auth[1]) {
-    try {
-      const data = toData(auth[1]);
-    } catch (e) {
-      res.status(400).send("Invalid JWT token");
+  try {
+    const images = await Images.findAll();
+    res.json(images);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { title, url } = req.body;
+    if (!title || !url) {
+      res.status(400).send("missing parameters");
+    } else {
+      const newImage = await Images.create(req.body);
+      res.json(newImage);
     }
-    const allImages = await Images.findAll();
-    res.json(allImages);
-  } else {
-    res.status(401).send({
-      message: "Please supply some valid credentials",
-    });
+  } catch (e) {
+    next(e);
   }
 });
 
